7189a49bf47f56eaacd34d985b49dfbe5ce3015f
hamid <hamid.dev@yahoo.com>
2020-12-23 16:53:23 +0330

    Update TwoFactorAuthTokenTest.php

--- tests/TwoFactorAuthTokenTest.php
+++ tests/TwoFactorAuthTokenTest.php
@@ -4,12 +4,18 @@
 namespace TwoFactorAuth\tests;
 
 
+use App\Models\User;
 use Tests\TestCase;
+use TwoFactorAuth\Facades\UserProviderFacade;
 
 class TwoFactorAuthTokenTest extends TestCase
 {
     public function test_sample()
     {
+        UserProviderFacade::shouldReceive('getUserByEmail')
+            ->once()
+            ->with('hamid@gmail.com')
+            ->andReturn(new User(['email' => 'hamid@gmail.com']));
         $response = $this->get('/api/two-factor-auth/request-token');
         $this->assertTrue($response->content() == 'hello');
     }
