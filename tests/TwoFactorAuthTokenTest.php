30dbb4c9d947fcb63af8c0454a557418239ade98
HamidRoohani <hamid.dev@yahoo.com>
2020-12-25 18:51:26 +0330

    Update TwoFactorAuthTokenTest.php

--- tests/TwoFactorAuthTokenTest.php
+++ tests/TwoFactorAuthTokenTest.php
@@ -6,6 +6,8 @@ namespace TwoFactorAuth\tests;
 
 use App\Models\User;
 use Tests\TestCase;
+use TwoFactorAuth\Facades\TokenGeneratorFacade;
+use TwoFactorAuth\Facades\TokenStoreFacade;
 use TwoFactorAuth\Facades\UserProviderFacade;
 
 class TwoFactorAuthTokenTest extends TestCase
@@ -15,7 +17,16 @@ class TwoFactorAuthTokenTest extends TestCase
         UserProviderFacade::shouldReceive('getUserByEmail')
             ->once()
             ->with('hamid@gmail.com')
-            ->andReturn(new User(['email' => 'hamid@gmail.com']));
+            ->andReturn($user = new User(['email' => 'hamid@gmail.com']));
+
+        TokenGeneratorFacade::shouldReceive('generateToken')
+            ->once()
+            ->withNoArgs()
+            ->andReturn('1q2w3e');
+
+        TokenStoreFacade::shouldReceive('tokenStore')
+            ->once()
+            ->with('1q2w3e',$user);
         $response = $this->get('/api/two-factor-auth/request-token');
         $this->assertTrue($response->content() == 'hello');
     }
